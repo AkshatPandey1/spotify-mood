23742fdd0044d928c9c86d647a775b9e8261d6af
AP <akshatpandeymyself@gmail.com>
2022-06-25 16:07:23 +0400

    Fixed permissions

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 function Login() {
-    const CLIENT_ID = process.env.REACT_APP_CID;
-    const REDIRECT_URI = process.env.REACT_APP_URI;
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-    const RESPONSE_TYPE = "token";
+  const CLIENT_ID = process.env.REACT_APP_CID;
+  const REDIRECT_URI = process.env.REACT_APP_URI;
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
 
-    return (
-        <div className={"div-size"}>
-            <h1>Spotify Profile Viewer</h1>
+  return (
+    <div className={"div-size"}>
+      <h1>Spotify Profile Viewer</h1>
 
-            <div className={"login"}>
-                <a
-                    href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-currently-playing,user-modify-playback-state`}
-                >
-                    Login to Spotify
-                </a>
-            </div>
-        </div>
-    );
+      <div className={"login"}>
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-currently-playing,user-modify-playback-state,user-top-read,user-read-recently-played`}
+        >
+          Login to Spotify
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
