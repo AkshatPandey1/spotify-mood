aa78baa1c25bb9549ea68e372662f194cab4d322
AP <akshatpandeymyself@gmail.com>
2022-06-29 21:26:14 +0400

    Fixed stuff

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -25,53 +25,55 @@ function Profile(props) {
     }, [props.token, setData]);
 
     return (
-        <div className={"profile-card"}>
-            {loaded ? (
-                <div>
-                    {userData.images && userData.images.length > 0 ? (
-                        <div style={{height: "150px", position: "relative"}}>
-                            <img
-                                src={userData.images[0].url}
-                                onClick={() => window.open(userData.uri)}
-                                style={{
-                                    borderRadius: "100%",
-                                    height: "150px",
-                                    width: "150px",
-                                }}
-                                alt={"Profile pic couldn't be loaded"}
-
-                            />
-                            <img src={"Spotify-logo.png"} alt={"Spotify-logo"} style={{
-                                height: "30px",
-                                width: "30px",
-                                position: "absolute",
-                                right: "126px",
-                                top: "1px"
-                            }}/>
-
-                        </div>
-                    ) : (
-                        <div
-                            style={{height: "150px", width: "100%", textAlign: "center"}}
-                        >
-                            <h1 style={{fontSize: "1.75rem", color: "white"}}>
-                                No <br/> profile <br/>
-                                picture
-                            </h1>
-                        </div>
-                    )}
-                    <h1
-                        style={{cursor: "pointer"}}
-                        onClick={() => window.open(userData.external_urls.spotify)}
-                    >
-                        {userData.display_name}
-                    </h1>
-                    <SongList token={props.token}/>
-                </div>
+      <div className={"profile-card"}>
+        {loaded ? (
+          <div>
+            {userData.images && userData.images.length > 0 ? (
+              <div style={{ height: "150px", position: "relative" }}>
+                <img
+                  src={userData.images[0].url}
+                  onClick={() => window.open(userData.uri)}
+                  style={{
+                    borderRadius: "100%",
+                    height: "150px",
+                    width: "150px",
+                  }}
+                  alt={"Profile pic couldn't be loaded"}
+                />
+                <img
+                  src={"Spotify-logo.png"}
+                  alt={"Spotify-logo"}
+                  style={{
+                    height: "30px",
+                    width: "30px",
+                    position: "absolute",
+                    right: "126px",
+                    top: "1px",
+                  }}
+                />
+              </div>
             ) : (
-                <h1>No data</h1>
+              <div
+                style={{ height: "150px", width: "100%", textAlign: "center" }}
+              >
+                <h1 style={{ fontSize: "1.75rem", color: "white" }}>
+                  No <br /> profile <br />
+                  picture
+                </h1>
+              </div>
             )}
-        </div>
+            <h1
+              style={{ cursor: "pointer" }}
+              onClick={() => window.open(userData.external_urls.spotify)}
+            >
+              {userData.display_name}
+            </h1>
+            <SongList token={props.token} />
+          </div>
+        ) : (
+          <h1>No data</h1>
+        )}
+      </div>
     );
 }
 
